0a9ac9675141312e95efefe865dc2ff5693d6cbf
Mohit Ranka <mohitranka@gmail.com>
2019-08-15 17:16:28 +0530

    [Automated commit] Thu Aug 15 11:46:28 UTC 2019

--- bin/starttmux.sh
+++ bin/starttmux.sh
@@ -5,22 +5,34 @@
 # create a new tmux session or attach tmux window
 [ -z "$TMUX" ] && ( tmux attach || tmux new-session )
 
-# Window 1 - Orientation
+# Orientation
+tmux selectw -t 1
+
+## Window - 1
 tmux splitw -h -p 70
 tmux selectp -t 2
 tmux splitw -v -p 30 
 
+## Window - 2
+tmux new-window
+
 # Window 1: Music
+tmux selectw -t 1
 tmux selectp -t 1
 tmux send-keys "music" C-m
-#tmux send-keys "open 1" C-m
-#tmux send-keys "all shuffle repeat" C-m
+tmux send-keys "open 1" C-m
+tmux send-keys "all shuffle repeat" C-m
 
 # Window 1: Email
+tmux selectw -t 1
 tmux selectp -t 2
-#tmux send-keys "email" C-m
-tmux send-keys "ls" C-m
+tmux send-keys "email" C-m
 
 # Window 1: Notes
+tmux selectw -t 1
 tmux selectp -t 3
-tmux-send-keys "notes" C-m
+tmux send-keys "notes" C-m
+
+# Go to window 2
+
+tmux selectw -t 2
